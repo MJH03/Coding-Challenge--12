ff82e4dee8f66cd9d01b098f216e89dd416845da
MJH03 <mjhandelsman@usf.edu>
2024-06-25 09:08:27 -0400

    Implement data parsing and filtering from csv

---
+++ example.js
@@ -0,0 +1,10 @@
+//U6899-7808
+//Step 1: Data Processing
+function main() {
+    d3.csv('../data/mock_stock_data.csv', d3.autoType).then( function(data){
+        var svg = d3.select("svg")
+        
+    
+    })
+
+}
